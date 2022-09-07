89863a7dbbd97b1af867122e7e634632eb7124a4
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-09-07 15:51:46 +0800

    feat: add index and detail page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const user = require('./modules/user')
+const todo = require('./modules/todo')
 
-
+router.use('/todos', todo)
 router.use('/user', user)
 router.use('/', home)
 
