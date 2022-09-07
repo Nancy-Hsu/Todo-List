c1eed3e7c26d01e8f8d515ba96fd45223d5920dd
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-09-08 00:04:43 +0800

    feat: add edit function

--- routes/modules/todo.js
+++ routes/modules/todo.js
@@ -4,6 +4,7 @@ const db = require('../../models')
 const Todo = db.Todo
 const User = db.User
 
+// show detail page
 router.get('/:id', (req, res) => {
   const id = req.params.id
   const userId = req.user.id
@@ -11,4 +12,31 @@ router.get('/:id', (req, res) => {
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
+
+// show edit page
+router.get('/:id/edit', (req, res) => {
+  const userId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { userId, id }})
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+// edit the data
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const userId = req.user.id
+  const { name, isDone } = req.body
+
+  return Todo.update({ name, isDone: isDone === 'on' }, { where: { id, userId }})
+    .then(() => res.redirect(`/todos/${ id }`))
+    .catch(error => console.log(error))
+
+    // return Todo.findOne({ where: { userId, id } })
+  // .then(todo => { 
+  //   return todo.update({ name, isDone: isDone === 'on' })
+  //     .then((updatedtodo) => res.render('detail', { todo: updatedtodo.toJSON() }))
+  // })
+})
+
 module.exports = router
