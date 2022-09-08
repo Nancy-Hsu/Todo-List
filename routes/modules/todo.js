444ec162e11becb19006a180ad8b7b38ee581f53
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-09-08 14:19:06 +0800

    feat: add delete todo function

--- routes/modules/todo.js
+++ routes/modules/todo.js
@@ -8,7 +8,7 @@ const User = db.User
 router.get('/:id', (req, res) => {
   const id = req.params.id
   const userId = req.user.id
-  return Todo.findOne({ where: { userId, id}})
+  return Todo.findOne({ where: { userId, id } })
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
@@ -17,7 +17,7 @@ router.get('/:id', (req, res) => {
 router.get('/:id/edit', (req, res) => {
   const userId = req.user.id
   const id = req.params.id
-  return Todo.findOne({ where: { userId, id }})
+  return Todo.findOne({ where: { userId, id } })
     .then(todo => res.render('edit', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
@@ -28,15 +28,25 @@ router.put('/:id', (req, res) => {
   const userId = req.user.id
   const { name, isDone } = req.body
 
-  return Todo.update({ name, isDone: isDone === 'on' }, { where: { id, userId }})
-    .then(() => res.redirect(`/todos/${ id }`))
+  return Todo.update({ name, isDone: isDone === 'on' }, { where: { id, userId } })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 
-    // return Todo.findOne({ where: { userId, id } })
+  // return Todo.findOne({ where: { userId, id } })
   // .then(todo => { 
   //   return todo.update({ name, isDone: isDone === 'on' })
   //     .then((updatedtodo) => res.render('detail', { todo: updatedtodo.toJSON() }))
   // })
+})
+
+//delete
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  const userId = req.user.id
+  return Todo.destroy({ where: { id, userId } })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+
 })
 
 module.exports = router
